57dfed1788a18a0647838291de9f91251ec259dc
Bhavani G R <bhavanibrgr@gmail.com>
2025-12-24 20:07:33 +0530

    Assignment 2

---
+++ PrimitiveDataTypes_ass1.java
@@ -0,0 +1,20 @@
+package assignment_DataTypes;
+
+public class PrimitiveDataTypes_ass1 {
+
+	public static void main(String[] args) {
+		
+		int age=22;
+		float height=170.5f;
+		long mobileno=9876543210l;
+		boolean value=true;
+		
+		System.out.println("Age:"+age);
+		System.out.println("Height:"+height);
+		System.out.println("Mobile number:"+mobileno);
+		System.out.println("Is Student:"+ value);
+		
+
+	}
+
+}
---
+++ PrimitiveDataTypes_ass2.java
@@ -0,0 +1,29 @@
+package assignment_DataTypes;
+
+public class PrimitiveDataTypes_ass2 {
+
+	public static void main(String[] args) {
+		byte b=10;
+		short s=100;
+		int i=5000;
+		long mobileno=9000000000l;
+		float height=5.5f;
+		double d=99.99;
+		char c='A';
+		boolean value=true;
+		
+		System.out.println("Byte value:"+b);
+		System.out.println("Short value:"+s);
+		System.out.println("Int value:"+i);
+		System.out.println("Long value:"+mobileno);
+		System.out.println("Float value:"+height);
+		System.out.println("Double value:"+d);
+		System.out.println("Char value:"+c);
+		System.out.println("Boolean value:"+value);
+		
+		
+		
+
+	}
+
+}
